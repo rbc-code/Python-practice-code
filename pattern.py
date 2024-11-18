69c66f39ad518e3480a212ef24fdf951a78fc5e6
RB Chauhan <chauhanrb08@gmail.com>
2024-11-18 21:31:46 +0530

    pattern

---
+++ pattern.py
@@ -0,0 +1,82 @@
+# n=int(input("Enter row no. "))
+# i=1
+# while i<=n:
+#     print('*' * i+(n-i)*' ')
+
+#     i=i+1
+#=======================================
+
+# n=int(input("Enter row no. "))
+# i=1
+# while i<=n:
+#     print((n-i)*' '+i*'*')
+
+#     i=i+1
+#=======================================
+
+# n=int(input("Enter row no. "))
+# i=0
+# while i<n:
+#     print((n-i)*'*'+' '*i)
+
+#     i=i+1
+
+#======================================
+
+# n=int(input("Enter row no. "))
+# i=0
+# while i<n:
+#     print(i*' '+'*'*(n-i))
+
+#     i=i+1
+#=======================================
+
+# n=int(input("Enter row no. "))
+# i=0
+# while i<=n:
+#     print('*'*i+' '*(n-1))
+#     i=i+1
+# i=i-2
+# while i>0:
+#     print('*'*i)
+#     i=i-1
+#================================================== 
+
+# n=int(input("Enter row no. "))
+# i=1
+# while i<=n:
+#     print(' '*(n-i)+'*'*i)
+#     i=i+1
+
+# i=i-2
+# while i>0:
+#     print((n-i)*' '+'*'*i)
+#     i=i-1    
+#=====================================================
+    
+
+# n=int(input("Enter row no. "))
+# i=0
+# while i<n:
+#     print((n-i)*'*'+' '*i)
+
+#     i=i+1
+
+# i=1
+# while i<=n:
+#     print('*' * i+(n-i)*' ')
+
+#     i=i+1
+
+#========================================================
+    
+n=int(input("Enter number"))
+i=n
+while i>0:
+    print('*'*i)
+    i=i-1
+print(i)
+i=i+2
+while i<=n:
+    print('*'*i)
+    i=i+1    
